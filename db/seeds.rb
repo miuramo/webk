331aea0c1095f7cc75e9e0c804c7ae79be2a0ef4
Motoki Miura <miuramo@mns.kyutech.ac.jp>
2017-11-27 16:29:58 +0900

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  User.create(name: "u#{i}", email: "u#{i}@example.com", pass: "u#{i}")
+end
+
+Url.create(name: "tobata.asia", url:"tobata.asia", user_id: 1)
+Url.create(name: "yahoo.co.jp", url:"yahoo.co.jp", user_id: 1)
+Url.create(name: "google.com", url:"google.com", user_id: 2)
+Url.create(name: "kyutech.ac.jp", url:"kyutech.ac.jp", user_id: 3)
+
+
+
