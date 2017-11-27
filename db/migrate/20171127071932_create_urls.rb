5606e2c921f695e22f8de722f5a8b60263b3b291
Motoki Miura <miuramo@mns.kyutech.ac.jp>
2017-11-27 16:19:45 +0900

    scaffold Url

---
+++ db/migrate/20171127071932_create_urls.rb
@@ -0,0 +1,11 @@
+class CreateUrls < ActiveRecord::Migration[5.1]
+  def change
+    create_table :urls do |t|
+      t.string :name
+      t.string :url
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
